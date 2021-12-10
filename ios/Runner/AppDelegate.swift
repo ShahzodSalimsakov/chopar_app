dd290063e69cb18e20153d2233ab4bf79f3703ba
Bobur Juraev <boburjuraev@Boburs-MacBook-Pro.local>
2021-12-10 23:28:51 +0500

    notification update

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,9 +9,6 @@ import YandexMapsMobile
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
-    }
     YMKMapKit.setApiKey("d26bf40f-1258-41f9-84fb-381fedc55b21")
     FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
